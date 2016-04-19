5e54cfa5139a1ea277583b2641efdce10950b122
Jason Lam <jasonlam604@gmail.com>
2016-04-18 22:14:42 -0700

    Comment example temporarily, invalid code

--- examples/basic/index.php
+++ examples/basic/index.php
@@ -5,8 +5,12 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 use Kozo\Web\Router\Route;
 use App\DummyObject;
 
+/* needs updating *
+
 $route =  new Route([
             "GET","DELETE"
         ], "/users/:number", "App\DummyObjectTwo");
 
 $route->execute($_SERVER['PATH_INFO']);
+
+*/
